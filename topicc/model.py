fix error with masking

topicc/model.py
@@ -63,10 +63,9 @@ class TopicC(nn.Module):
         enc_outputs, _ = nn.utils.rnn.pad_packed_sequence(enc_outputs)
 
         # encoder masks to indicate which parts of the sequence should be considered
-        enc_masks = torch.zeros(enc_outputs.shape[0], enc_outputs.shape[1], 1, dtype=torch.float)
+        enc_masks = torch.zeros(enc_outputs.shape[0], enc_outputs.shape[1], 1, dtype=torch.bool)
         for i, last in enumerate(seq_last):
-            seq_len = last + 1
-            enc_masks[i, seq_len:, 0] = True
+            enc_masks[(last + 1):, i,  0] = True
 
         # att_vec.shape = max_seq_len, batch_size, attention_size
         att_vec = self.enc_to_att_map(enc_outputs)
@@ -75,7 +74,7 @@ class TopicC(nn.Module):
         # pointer.shape = max_seq_len, batch_size, 1
         pointer = self.att_to_pointer_map(att_vec)
         # mask out sections which are not part of the sequence
-        pointer.data.masked_fill_(enc_masks, -float('inf'))
+        pointer = pointer.masked_fill(enc_masks, -float('inf'))
         # pointer_w.shape = max_seq_len, batch_size, 1
         # each slice pointer_w[:, seq_n, 0] will sum to 1, where the values
         # indicate where the most attention should be paid

topicc/optimiser.py
@@ -6,11 +6,11 @@ from topicc.data import train_test_split
 
 def train(topicc: TopicC, dataset: WikiVALvl5Dataset) -> TopicC:
     # constants TODO: move to args
-    lr = 0.001
+    lr = 0.005
     epochs = 1
     batch_size = 32
     clip_grad = 5
-    report_batch = 10
+    report_batch = 20
 
     # set up the batch data
     # TODO: train/test split
@@ -29,11 +29,11 @@ def train(topicc: TopicC, dataset: WikiVALvl5Dataset) -> TopicC:
 
     for i_epoch in range(epochs):
         for i_batch, (sequences, labels) in enumerate(dataloader):
+            optimizer.zero_grad()
             loss = topicc.loss(sequences, labels)
             current_loss += loss
-            optimizer.zero_grad()
             loss.backward()
-            # _ = torch.nn.utils.clip_grad_norm_(topicc.parameters(), clip_grad)
+            _ = torch.nn.utils.clip_grad_norm_(topicc.parameters(), clip_grad)
             optimizer.step()
 
             if i_batch % report_batch == 0: